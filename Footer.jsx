ec5f6d97bb8f2d95aedf7bcb618209a184e54571
Willpower Group <ourwillpower@gmail.com>
2026-01-21 23:45:23 +0530

    Add Footer component with copyright notice

---
+++ Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center bg-black text-white text-sm">
+      © {new Date().getFullYear()} Adfirm Media. All rights reserved.
+    </footer>
+  );
+}
