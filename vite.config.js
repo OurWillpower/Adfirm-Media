3843a3c226c81fbce212a10d1567239d7487557d
Willpower Group <ourwillpower@gmail.com>
2026-01-21 11:43:36 +0530

    Add Vite configuration for React project

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // This must match your new repository name exactly
+  base: '/adfirm-media/',
+})
