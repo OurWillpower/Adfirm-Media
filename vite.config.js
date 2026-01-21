77dc0d79708d7cba2a8756079b2f31af283b87f7
Willpower Group <ourwillpower@gmail.com>
2026-01-21 23:34:14 +0530

    Change base URL to relative path
    Updated base URL in Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Based on your screenshot, your URL uses Capital Letters. 
-  // This line MUST match that exactly.
-  base: '/Adfirm-Media/',
-})
+  base: "./"
+});
