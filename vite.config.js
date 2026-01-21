0441b1728e46f87614d79ea3ddfa52b32e3d74d8
Willpower Group <ourwillpower@gmail.com>
2026-01-21 18:58:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This must match your repository URL capitalization exactly
+  // Based on your screenshot, your URL uses Capital Letters. 
+  // This line MUST match that exactly.
   base: '/Adfirm-Media/',
 })
