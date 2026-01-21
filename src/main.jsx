7d0c8a86dd119ad7466a4bae7c9041457070e9a9
Willpower Group <ourwillpower@gmail.com>
2026-01-21 19:49:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// I have removed the import './index.css' line to prevent the crash
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
